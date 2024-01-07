0797aa3b620942a39874eeccdfc64e18cdee9bfd
20220698 <a.raudvee@student.tue.nl>
2024-01-07 03:49:12 +0100

    corrections in vectorizers

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 path_to_data_folder = '/Users/alexraudvee/Desktop/lai-data'
 path_to_fast_text_model = '/Users/alexraudvee/Desktop/lai_models/cc.ar.300.bin'
+glove_path = '/Users/alexraudvee/Desktop/lai_models/glove.840B.300d.pkl'
